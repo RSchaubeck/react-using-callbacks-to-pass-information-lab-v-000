28806e93e06d1804ed835cd3e5932a8994f6254c
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-19 02:58:39 +0000

    Automatically backed up by Learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -6,7 +6,7 @@ export default class ColorSelector extends Component {
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
       const colorChoice = (event) => {<Matrix chosenColor={str}/>}
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={colorChoice}/>
+      <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={colorChoice}/>
       console.log(colorChoice())
     })
   )
