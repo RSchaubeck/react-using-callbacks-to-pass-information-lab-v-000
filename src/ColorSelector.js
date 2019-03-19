9040becdf1628a1e052369660649babf08d2a214
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-19 04:02:46 +0000

    Automatically backed up by Learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Matrix from './Matrix.js'
 
 export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick=<Matrix chosenColor`${str}` />/>
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick= />
     })
   )
 
