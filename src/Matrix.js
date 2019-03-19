f5868d3d3465e9069e62605e401e757d4b822360
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-19 03:31:56 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -14,7 +14,7 @@ export default class Matrix extends Component {
 
   chosenColor = (color) => {
     this.setState({
-      selectedColor: color
+      selectedColor: this.props.color
     })
   }
 
